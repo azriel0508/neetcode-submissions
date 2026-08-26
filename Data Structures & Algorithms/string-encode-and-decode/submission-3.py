381a4a4bb789e4a5a65799050c6589b022fb1552
Felix Azriel Elmido <felix.elmido27@gmail.com>
2026-08-26 10:22:47 +0100

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    #Input: List of strings
+    #Output: Encode function outputs an encoded string, decode brings back the input of the array of strings
+    #Brute Force Method: We split the strings using one character and when decoding it we use a nested while loop
+    #Optimised Solution:
+    def encode(self, strs: List[str]) -> str:
+        s = ""
+        for word in strs:
+            s += str(len(word)) + "#" + word
+
+        return s
+    def decode(self, s: str) -> List[str]:
+        ans = []
+        i, j = 0, 0 
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+            length = int(s[i:j])
+            start = j + 1
+            end = start + length
+            i = end
+            ans.append(s[start:end])
+        
+        return ans
